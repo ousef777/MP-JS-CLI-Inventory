ea0b57ea254caa0ff3dc5fec20ecf846f14393cd
Octowl <alsaff1987@gmail.com>
2024-11-07 03:12:43 +0300

    data source

---
+++ data/books.js
@@ -0,0 +1,13 @@
+import { v4 as uuid4 } from "uuid";
+
+const books = [
+  {
+    id: uuid4(),
+    title: "The Lord of the Rings",
+    author: "J. R. R. Tolkein",
+    genre: "Fantasy",
+    published: 1954,
+  },
+];
+
+export default books;
